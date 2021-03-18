8d77e2820dc8b2379af9855ace32ff3c847ab217
ben9583 <bplate9583@gmail.com>
2021-03-18 14:56:52 -0700

    add version command

--- src/main/java/ben9583/discord/commands/Ping.java
+++ src/main/java/ben9583/discord/commands/Ping.java
@@ -9,7 +9,7 @@ import com.github.rainestormee.jdacommand.CommandHandler;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 
-@CommandDescription(name = "ping", triggers = { "ping" }, description = "Pings the bot (for debug purposes).", attributes = {
+@CommandDescription(name = "ping", triggers = { "ping" }, description = "Pings the bot (for debug purposes)", attributes = {
         @CommandAttribute(key = "AdminOnly", value = "1") })
 public class Ping implements AbstractCommand<Message> {
     public Ping() { }
---
+++ src/main/java/ben9583/discord/commands/Version.java
@@ -0,0 +1,18 @@
+package ben9583.discord.commands;
+
+import com.github.rainestormee.jdacommand.AbstractCommand;
+import com.github.rainestormee.jdacommand.CommandAttribute;
+import com.github.rainestormee.jdacommand.CommandDescription;
+import net.dv8tion.jda.api.entities.Message;
+
+@CommandDescription(name = "version", triggers = { "version" }, description = "Returns the version of this plugin", attributes = {
+        @CommandAttribute(key = "AdminOnly", value = "1") })
+public class Version implements AbstractCommand<Message> {
+    public Version() { }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void execute(Message message, String args) {
+        message.getChannel().sendMessage("Running version 1.3.0-alpha2");
+    }
+}
