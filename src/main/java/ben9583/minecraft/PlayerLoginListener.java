0fa51916468f4c1b09d6db12e0ffe28c26210551
ben9583 <bplate9583@gmail.com>
2021-03-19 13:56:46 -0700

    grammar fix

--- src/main/java/ben9583/minecraft/PlayerLoginListener.java
+++ src/main/java/ben9583/minecraft/PlayerLoginListener.java
@@ -24,16 +24,16 @@ public class PlayerLoginListener implements Listener {
         String uuid = event.getPlayer().getUniqueId().toString();
         if (this.whitelistHandler.checkIfWhitelistContains(uuid) && this.whitelistHandler.checkIfAuthDone(uuid)) {
             if (!this.whitelistHandler.checkIfAllowedToLogin(uuid)) {
-                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "You are not allowed to login. Please contact to the administrator.");
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "You are not allowed to login; contact a server administrator if you believe this is an error.");
             } else if (!this.whitelistHandler.checkIfAccessVoteCountPositive(uuid)) {
-                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "You are not allowed to login. Please wait that your vote count is positive in the access request channel.");
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "You are not allowed to login; make sure your vote count is positive in the access-request Discord channel.");
             } else if (!this.whitelistHandler.checkIfConfirmedIpForUser(uuid, event.getRealAddress().getHostAddress())) {
                 String authToken = this.whitelistHandler.startIPAuthProcess(uuid, event.getRealAddress().getHostAddress());
-                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Unknown IP Address for that user, please send following number as private message to the Discord bot:  " + authToken);
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Unknown IP address for this user; please send the following number as private message to the Discord bot: " + authToken);
             }
         } else {
             String authToken = this.whitelistHandler.startAuthProcess(event.getPlayer().getName(), uuid, event.getRealAddress().getHostAddress());
-            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Start auth process, send following number as private message to the Discord Bot: " + authToken);
+            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Your auth token has been created; please send the following number as private message to the Discord Bot: " + authToken);
         }
 
 
