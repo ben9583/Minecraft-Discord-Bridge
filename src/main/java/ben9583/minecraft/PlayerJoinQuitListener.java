bad11e40fe201ee4dd29f99708888f19d11afd6a
ben9583 <bplate9583@gmail.com>
2021-03-23 09:15:16 -0700

    remove redundant method call

--- src/main/java/ben9583/minecraft/PlayerJoinQuitListener.java
+++ src/main/java/ben9583/minecraft/PlayerJoinQuitListener.java
@@ -21,7 +21,7 @@ public class PlayerJoinQuitListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void playerJoin(PlayerJoinEvent event) {
         if(this.server.getOnlinePlayers().size() == 1) {
-            bot.sendMessageToChannel(event.getPlayer().getName() + " logged in. There is now " + this.server.getOnlinePlayers().size() + " player online.");
+            bot.sendMessageToChannel(event.getPlayer().getName() + " logged in. There is now 1 player online.");
         } else {
             bot.sendMessageToChannel(event.getPlayer().getName() + " logged in. There are now " + this.server.getOnlinePlayers().size() + " players online.");
         }
